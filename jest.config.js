7b84eb8ff155517cc3e456e07bed9c30a1f1e98e
Kirill <marchenko@finleo.ru>
2024-05-18 23:22:07 +0300

    feat: write test for button

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ export default {
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,7 +1,37 @@
 import { render, screen } from "@testing-library/react";
-import { Button } from ".";
+import { Button } from "./index";
 
-test("renders button with text", () => {
-  render(<Button>Click Me</Button>);
-  expect(screen.getByText("Click Me")).toBeInTheDocument();
+const text = "my ui button";
+
+describe("ui button", () => {
+  it("should render button with children text", () => {
+    render(<Button>{text}</Button>);
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+
+  it("should render button critical variant", () => {
+    render(<Button variant="filled">{text}</Button>);
+
+    const button = screen.getByText(text);
+
+    expect(button).toHaveStyle("background-color: #6765F2");
+    expect(button).toHaveStyle("color: #FFFFFF");
+  });
+
+  it("should render button with upper case text", () => {
+    render(<Button textIsUpper>{text}</Button>);
+
+    const button = screen.getByText(text);
+
+    expect(button).toHaveStyle("text-transform: uppercase");
+  });
+
+  it("should render button without uppercase", () => {
+    render(<Button textIsUpper={false}>{text}</Button>);
+
+    const button = screen.getByText(text);
+
+    expect(button).not.toHaveStyle("text-transform: uppercase");
+  });
 });
